dd6a9217382070a5ce53a8f6420de2594d8cadd5
Anthony <dev.autumnsky@gmail.com>
2021-02-21 19:55:18 +0900

    input and async-await testing

--- 05.simulating-user-input/tests/unit/example.spec.js
+++ 05.simulating-user-input/tests/unit/example.spec.js
@@ -1,9 +1,28 @@
 import { shallowMount } from "@vue/test-utils"
 import FormSubmitter from "@/components/FormSubmitter.vue"
+// 모든 pending 상태의 Promise를 즉시 resolve
+import flushPromises from "flush-promises"
+
+let url = ''
+let data = ''
+
+const mockHttp = {
+  get: (_url, _data) => {
+    return new Promise((resolve, reject) => {
+      url = _url
+      data = _data
+      resolve()
+    })
+  }
+}
 
 describe("FormSubmitter", () => {
   it("제출했을 때 알림이 나타난다", async () => {
-    const wrapper = shallowMount(FormSubmitter)
+    const wrapper = shallowMount(FormSubmitter, {
+      mocks: {
+        $http: mockHttp
+      }
+    })
 
     // username이라는 data 변수를 찾고, alice라는 값으로 설정
     wrapper.find("[data-username]").setValue("alice")
@@ -11,10 +30,17 @@ describe("FormSubmitter", () => {
     wrapper.find("form").trigger("submit.prevent")
 
     // 비동기 처리 : 반응성 시스템을 통해 DOM이 업데이트 되었는지 확인하는 로직
-    await wrapper.vm.$nextTick()
+    // await wrapper.vm.$nextTick()
+
+    await flushPromises()
 
     // 클래스 message 가 적용된 요소를 찾고, 텍스트를 반환한다.
     expect(wrapper.find(".message").text())
       .toBe("Thank you for your submission, alice.")
+
+
+    // 엔드포인트 및 페이로드 검사
+    expect(url).toBe("/api/v1/register")
+    expect(data).toEqual({ username: "alice" })
   })
 })
