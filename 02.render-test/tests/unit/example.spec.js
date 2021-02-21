1494b69c569f52f53ba7f816a8f3430e54560ee3
Anthony <dev.autumnsky@gmail.com>
2021-02-21 19:49:59 +0900

    Add comment shallow mount

--- 02.render-test/tests/unit/example.spec.js
+++ 02.render-test/tests/unit/example.spec.js
@@ -3,7 +3,9 @@ import Parent from '@/components/Parent.vue'
 
 describe('Parent.vue', () => {
   it('renders props.msg when passed', () => {
+    // shallow mount : 자식 요소는 랜더 하지 않는다 (테스트 환경을 격리 할 수 있다)
     const shallowWrapper = shallowMount(Parent)
+    // mount : 모든 요소를 랜더
     const mountWrapper = mount(Parent)
 
     console.log(shallowWrapper.html())
